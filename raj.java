3fc4d52d59b4a2fb3ebcb311da8bdae22e536e08
unknown <harshithxworkz@gmail.com>
2021-12-20 22:54:18 +0530

    learning example 9

---
+++ raj.java
@@ -0,0 +1,29 @@
+class raj { 							                                                                                                                                                                 								
+         public static void main (String[]args){
+        // datatype rvn =value;
+       byte bags= 20;
+    System.out.println(bags);
+   
+    short capacity= 300;
+     System.out.println(capacity);
+ 
+     long  networth = 1000000000l;  
+     System.out.println(networth);
+
+    char box = 'h';
+     System.out.println(box);
+       
+     int stars = 1000;
+    System.out.println(stars);
+   
+     float cgpa =6.33f;
+     System.out.println(cgpa);
+   
+      double income= 9.03;
+    System.out.println(income);
+
+    boolean water= true;
+   System.out.println(water);
+   }
+}
+       
